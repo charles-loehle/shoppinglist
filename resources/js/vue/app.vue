4238043b8cc0f37c5e5fff02d2d3f6d1a19d2a8d
Charles Loehle <clguitar1@gmail.com>
2021-12-31 17:40:08 -0500

    initial commit

---
+++ resources/js/vue/app.vue
@@ -0,0 +1,51 @@
+<template>
+    <div class="container">
+        <div class="heading">
+            <h2 class="title">Shopping List</h2>
+            <add-item-form v-on:reloadlist="getList()" />
+        </div>
+        <list-view :items="items" v-on:reloadlist="getList()" />
+    </div>
+</template>
+
+<script>
+import addItemForm from "./addItemForm.vue";
+import listView from "./listView.vue";
+export default {
+    components: {
+        addItemForm,
+        listView,
+    },
+    data() {
+        return {
+            items: [],
+        };
+    },
+    methods: {
+        getList() {
+            axios
+                .get("api/items")
+                .then((response) => (this.items = response.data))
+                .catch((error) => console.error(error));
+        },
+    },
+    created() {
+        this.getList();
+    },
+};
+</script>
+<style scoped>
+.container {
+    width: 400px;
+    margin: auto;
+}
+
+.heading {
+    background: #e6e6e6;
+    padding: 10px;
+}
+
+.title {
+    text-align: center;
+}
+</style>
